0ad9a2af3c8e6d3f3d17c785e5e2f41adaa32b7d
Raj Posture <rajposture16@gmail.com>
2026-05-19 23:18:29 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,69 +1,85 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
-console.log("ENV CHECK:", {
-  STREAM_API_KEY: ENV.STREAM_API_KEY,
-  STREAM_API_SECRET: ENV.STREAM_API_SECRET,
-});
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoute.js";
 import executeRoute from "./routes/executeRoute.js";
+
 const app = express();
-
 const __dirname = path.resolve();
-
-app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://intervue.site/",
+  "https://intervue.site",
+  "https://www.intervue.site",
 ];
+
+app.use(express.json());
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); 
-
-      if (allowedOrigins.includes(origin)) {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
         return callback(null, true);
-      } else {
-        return callback(new Error("CORS not allowed"));
       }
+
+      return callback(new Error("CORS not allowed"));
     },
     credentials: true,
   })
 );
-app.use(clerkMiddleware()); 
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
+
+app.use(clerkMiddleware());
+
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/execute", executeRoute);
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "api is up and running" });
+
+app.get("/health", (_, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running",
+  });
 });
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server running on port ${ENV.PORT}`);
+    });
   } catch (error) {
-    console.error("Error starting the server", error);
+    console.error("Server startup failed:", error);
+    process.exit(1);
   }
 };
 
